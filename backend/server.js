ed8f07045f0d9ef124b42ca645069ad895ce148d
Anthony Perez <43166263+anthonypz@users.noreply.github.com>
2023-05-22 14:09:44 -0700

    Create express routes

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5001;
+import products from "./data/products.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Api is running");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () =>
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${port}`
+  )
+);
