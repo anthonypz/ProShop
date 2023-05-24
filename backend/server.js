23e923b5e453c4e7415bc75e8093bb395eb6f3b4
Anthony Perez <43166263+anthonypz@users.noreply.github.com>
2023-05-23 23:35:18 -0700

    Create error middleware

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-const port = process.env.PORT || 5001;
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+const port = process.env.PORT || 5001;
 
 connectDB(); // Connect to MongoDB
 const app = express();
@@ -13,6 +14,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(
