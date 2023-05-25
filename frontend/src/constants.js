fe95a06ff1e452b3b8b66cddeb182f961476f716
Anthony Perez <43166263+anthonypz@users.noreply.github.com>
2023-05-25 16:53:15 -0700

    Use redux to fetch component data

---
+++ frontend/src/constants.js
@@ -0,0 +1,8 @@
+// Uncomment below if you are not using a proxy
+// export const BASE_URL =
+//   import.meta.env.MODE === "development" ? "http://localhost:5001" : "";
+export const BASE_URL = "";
+export const PRODUCTS_URL = "/api/products";
+export const USERS_URL = "/api/users";
+export const ORDERS_URL = "/api/orders";
+export const PAYPAL_URL = "/api/config/paypal";
