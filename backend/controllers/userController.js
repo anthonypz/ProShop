1f8632e8511176eb8c8c778e80830259ebaa8386
Anthony Perez <43166263+anthonypz@users.noreply.github.com>
2023-05-28 23:36:41 -0700

    Fix: send response to generateToken function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    generateToken(req, user._id);
+    generateToken(res, user._id);
 
     res.json({
       _id: user._id,
@@ -74,7 +74,19 @@ const logoutUser = (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("get user profile");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Update user profile
