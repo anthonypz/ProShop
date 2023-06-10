25e3ef2274ac00124b2afcf83659f1d672948776
Anthony P <43166263+anthonypz@users.noreply.github.com>
2023-06-10 05:36:15 -0700

    feat: carousel top products showcase

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -17,7 +17,9 @@ const HomeScreen = () => {
 
   return (
     <>
-      {keyword && (
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
         <Link
           to="/"
           className="btn btn-light mb-4"
