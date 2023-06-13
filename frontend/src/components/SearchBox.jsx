816b468ab8a1975923683b94c26f1f85104fbb97
Anthony P <43166263+anthonypz@users.noreply.github.com>
2023-06-12 23:15:16 -0700

    fix: uncontrolled input on urlKeyword

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,41 +1,41 @@
-import { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const SearchBox = () => {
   const navigate = useNavigate();
   const { keyword: urlKeyword } = useParams();
-  const [keyword, setKeyword] = useState(urlKeyword);
+  const [keyword, setKeyword] = useState(urlKeyword || '');
 
   const submitHandler = (e) => {
     e.preventDefault();
     if (keyword) {
       navigate(`/search/${keyword.trim()}`);
-      setKeyword("");
+      setKeyword('');
     } else {
-      navigate("/");
+      navigate('/');
     }
   };
 
   return (
     <Form
       onSubmit={submitHandler}
-      className="d-flex"
+      className='d-flex'
     >
       <Form.Control
-        type="text"
-        name="q"
+        type='text'
+        name='q'
         onChange={(e) => setKeyword(e.target.value)}
         value={keyword}
-        placeholder="Search Products..."
-        className="mr-sm-2 ml-sm-5"
+        placeholder='Search Products...'
+        className='mr-sm-2 ml-sm-5'
       ></Form.Control>
       <Button
-        type="submit"
-        variant="outline-light"
-        className="p-2 mx-2"
+        type='submit'
+        variant='outline-light'
+        className='p-2 mx-2'
       >
         Search
       </Button>
