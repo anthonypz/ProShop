0fdb1088ab4961cd465f182985bf3fbac78f472d
Anthony P. <43166263+anthonypz@users.noreply.github.com>
2023-08-23 14:51:59 -0700

    add padding to product image

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,42 +1,43 @@
-import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
-import Rating from "./Rating";
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
-  return (
-    <Card className="my-3 rounded">
-      <Link to={`/product/${product._id}`}>
-        <Card.Img
-          src={product.image}
-          variant="top"
-        />
-      </Link>
+	return (
+		<Card className='my-3 rounded'>
+			<Link to={`/product/${product._id}`}>
+				<Card.Img
+					src={product.image}
+					variant='top'
+					className='p-2'
+				/>
+			</Link>
 
-      <Card.Body>
-        <Link to={`/product/${product._id}`}>
-          <Card.Title
-            as="div"
-            className="product-title mb-0"
-          >
-            <strong>{product.name}</strong>
-          </Card.Title>
-        </Link>
+			<Card.Body>
+				<Link to={`/product/${product._id}`}>
+					<Card.Title
+						as='div'
+						className='product-title mb-0'
+					>
+						<strong>{product.name}</strong>
+					</Card.Title>
+				</Link>
 
-        <Card.Text as="div">
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
-        </Card.Text>
+				<Card.Text as='div'>
+					<Rating
+						value={product.rating}
+						text={`${product.numReviews} reviews`}
+					/>
+				</Card.Text>
 
-        <Card.Text
-          as="h3"
-          className="mt-1"
-        >
-          ${product.price}
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  );
+				<Card.Text
+					as='h3'
+					className='mt-1'
+				>
+					${product.price}
+				</Card.Text>
+			</Card.Body>
+		</Card>
+	);
 };
 export default Product;
